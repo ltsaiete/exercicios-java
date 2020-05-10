45f48373ae5604373e6e9c9b58b7a3a6d7a9ecb7
Luis Saiete <luistsaiete@gmail.com>
2020-05-10 22:07:11 +0200

    fist commit

---
+++ introducao/HelloWorld/src/main/java/com/mycompany/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.helloworld;
+
+import javax.swing.JOptionPane;
+
+/**
+ * @author LEWIS
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null, "Hello World", "Message", JOptionPane.INFORMATION_MESSAGE);
+    }
+    
+}
